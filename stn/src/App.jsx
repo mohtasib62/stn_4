52a2326d80902d840a816dd031ba4d8ddbce9b1a
Mohtasib <atifkhan3029965@gmail.com>
2025-08-13 16:00:43 +0100

    main code changed

--- stn/src/App.jsx
+++ stn/src/App.jsx
@@ -1,34 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import Header from './Header'
+import { BrowserRouter,
+  Routes,
+  Route
+    } from 'react-router-dom';
+  import About from './About';
+import Client from './Client';
+import Home from './Home';
+import Footer from './Footer'
+import Recruitment from './Recruitment';
+import Policies from './Policies'
+import Contact from './Contact' 
+import Jobs from './Jobs';
+import Submit from './Submit'
+import Manager from './Manager';
+import Account from './Account';
+import Business from './Business';
+import Three from './Three';
+import Develop from './Develop';
+
+
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+   <BrowserRouter>
+   <Header />
+   <div className="app">
+   <Routes>
+    <Route path='/' element={<Home />}></Route>
+    <Route path='develop' element={<Develop />}></Route>
+    <Route path='three' element={<Three />}></Route>
+    <Route path='business' element={<Business />}></Route>
+    <Route path='account' element={<Account />}></Route>
+    <Route path='manager' element={<Manager />}></Route>
+    <Route path='submit' element={<Submit />}></Route>
+    <Route path='jobs' element={<Jobs />}></Route>
+    <Route path='contact' element={<Contact />}></Route>
+    <Route path='recruitment' element={<Recruitment />}></Route>
+    <Route path='about' element={<About />}></Route>
+    <Route path='policies' element={<Policies />}></Route>
+    <Route path='client' element={<Client />}></Route>
+   </Routes>
+   </div>
+   <Footer />
+   </BrowserRouter>
   )
 }
 
